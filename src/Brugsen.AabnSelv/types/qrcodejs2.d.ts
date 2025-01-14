1b12fec935395b5e3de7f4d854ccdc2c34d50488
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-01-14 13:23:08 +0100

    Fix qrcodejs2 typings

--- src/Brugsen.AabnSelv/types/qrcodejs2.d.ts
+++ src/Brugsen.AabnSelv/types/qrcodejs2.d.ts
@@ -1,1 +1,16 @@
-declare module 'qrcodejs2';
+declare module "qrcodejs2" {
+  export default class QRCode {
+    constructor(element: HTMLElement, text: string);
+    constructor(element: HTMLElement, options: Options);
+    makeCode(text: string): void;
+    clear(): void;
+  }
+
+  export interface Options {
+    text: string;
+    width?: number;
+    height?: number;
+    colorDark?: string;
+    colorLight?: string;
+  }
+}
