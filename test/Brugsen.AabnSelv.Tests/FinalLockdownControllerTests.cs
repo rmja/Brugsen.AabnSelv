da8dd32b471f932824f4db5a96e49ec7997aacd8
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-01-17 15:22:56 +0100

    Update FinalLockdownControllerTests.cs

--- test/Brugsen.AabnSelv.Tests/FinalLockdownControllerTests.cs
+++ test/Brugsen.AabnSelv.Tests/FinalLockdownControllerTests.cs
@@ -40,8 +40,10 @@ public class FinalLockdownControllerTests
         Assert.Equal(LockState.Unknown, _lockGadget.State);
         Assert.Equal(AlarmState.Unknown, _alarmGadget.State);
 
+        // Morning
+
         // When
-        _fakeTime.SetLocalNow(new DateTime(2025, 01, 16, 06, 00, 00)); // Morning - open
+        _fakeTime.SetLocalNow(new DateTime(2025, 01, 16, 06, 00, 00));
         var nextTick = await _controller.TickAsync(extendedSchedule, CancellationToken.None);
         Assert.Equal(LightState.Unknown, _lightGadget.State);
         Assert.Equal(LockState.Unknown, _lockGadget.State);
@@ -63,5 +65,26 @@ public class FinalLockdownControllerTests
         Assert.Equal(LockState.Locked, _lockGadget.State);
         Assert.Equal(AlarmState.Armed, _alarmGadget.State);
         Assert.Equal(new DateTime(2025, 01, 17, 23, 10, 00), nextTick);
+
+        // Next day
+        _lightGadget.State = LightState.Unknown;
+        _lockGadget.State = LockState.Unknown;
+        _alarmGadget.State = AlarmState.Unknown;
+
+        // When
+        _fakeTime.SetLocalNow(nextTick.Value); // Time to turn off the light
+        nextTick = await _controller.TickAsync(extendedSchedule, CancellationToken.None);
+        Assert.Equal(LightState.Off, _lightGadget.State);
+        Assert.Equal(LockState.Unknown, _lockGadget.State);
+        Assert.Equal(AlarmState.Unknown, _alarmGadget.State);
+        Assert.Equal(new DateTime(2025, 01, 17, 23, 15, 00), nextTick);
+
+        // When
+        _fakeTime.SetLocalNow(nextTick.Value); // Time to arm the alarm
+        nextTick = await _controller.TickAsync(extendedSchedule, CancellationToken.None);
+        Assert.Equal(LightState.Off, _lightGadget.State);
+        Assert.Equal(LockState.Locked, _lockGadget.State);
+        Assert.Equal(AlarmState.Armed, _alarmGadget.State);
+        Assert.Equal(new DateTime(2025, 01, 18, 23, 10, 00), nextTick);
     }
 }
