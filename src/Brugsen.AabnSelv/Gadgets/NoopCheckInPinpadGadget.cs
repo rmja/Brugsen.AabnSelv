0bcf5093e38bcc9e492c347e471accccad4a61ad
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2025-02-06 13:13:10 +0100

    Add warnings when using fake pinpad gadgets

--- src/Brugsen.AabnSelv/Gadgets/NoopCheckInPinpadGadget.cs
+++ src/Brugsen.AabnSelv/Gadgets/NoopCheckInPinpadGadget.cs
@@ -4,4 +4,9 @@ public class NoopCheckInPinpadGadget : ICheckInPinpadGadget
 {
     public string GadgetId { get; } = "noop-check-in-pinpad";
     public GadgetEntity GadgetEntity => GadgetEntity.CheckInPinpad;
+
+    public NoopCheckInPinpadGadget(ILogger<NoopCheckInPinpadGadget>? logger = null)
+    {
+        logger?.LogWarning("Using fake noop check-in pinpad");
+    }
 }
--- src/Brugsen.AabnSelv/Gadgets/NoopCheckOutPinpadGadget.cs
+++ src/Brugsen.AabnSelv/Gadgets/NoopCheckOutPinpadGadget.cs
@@ -4,4 +4,9 @@ public class NoopCheckOutPinpadGadget : ICheckOutPinpadGadget
 {
     public string GadgetId { get; } = "noop-check-out-pinpad";
     public GadgetEntity GadgetEntity => GadgetEntity.CheckOutPinpad;
+
+    public NoopCheckOutPinpadGadget(ILogger<NoopCheckOutPinpadGadget>? logger = null)
+    {
+        logger?.LogWarning("Using fake noop check-out pinpad");
+    }
 }
